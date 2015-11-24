25d910eb25fe7069eacca7f08fe6fa1aa8b04b61
Chris <chris@linux-1anr.site>
2015-11-24 11:22:21 +0100

    Feritg

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -12,6 +12,11 @@ struct colloid{
 void init(colloid* const c, const int N);
 void print(const colloid* const c, const int N, const string fname);
 
+void generate_steps(int* rx, int* ry, const int N);
+void go_steps(colloid* const c, int* rx, int* ry, const int N);
+void do_statistics(colloid* const c, double& meanx, double& meany, double& var, const int N);
+
+
 int main(void){
   
     int N  = 50;
@@ -44,9 +49,10 @@ int main(void){
     
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
-	    // call to function which randomly sets up rx and ry
-	    // call to function which pushes all colloids according to rx and ry
-	    // call to function which evaluates statistics
+		generate_steps(rx, ry, N);
+		go_steps(c, rx, ry, N);
+		do_statistics(c, meanx, meany, var, N);
+		
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
 	    stat << meany << "\t" << var << endl;
 	}
@@ -74,4 +80,37 @@ void print(const colloid* const c, const int N, const string fname){
     for(int i = 0; i < N; i++)
 	out << c[i].x << "\t" << c[i].y << endl;
     out.close();
+}
+
+void generate_steps(int* rx, int* ry, const int N){
+	for( int i=0; i<N; i++){
+		//generate random int having with equal prob. either -1, 0 or +1
+		rx[i] = int (3*(double(rand())/RAND_MAX)) - 1;
+		ry[i] = int (3*(double(rand())/RAND_MAX)) - 1;
+	}
+}
+
+void go_steps(colloid* const c, int* rx, int* ry, const int N){
+	for( int i=0; i<N; i++){
+		c[i].x += rx[i];
+		c[i].y += ry[i];
+	}
+}
+
+void do_statistics(colloid* const c, double& meanx, double& meany, double& var, const int N){
+	meanx = 0;
+	meany = 0;
+	var = 0;
+	
+	for( int i=0; i<N; i++){
+		meanx += c[i].x;
+		meany += c[i].y;
+	}
+	meanx /= N;
+	meany /= N;
+	
+	for( int i=0; i<N; i++){
+		var += (c[i].x-meanx)*(c[i].x-meanx) + (c[i].y-meany)*(c[i].y-meany);
+	}
+	var /= N;
 }
